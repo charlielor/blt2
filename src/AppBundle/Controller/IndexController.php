83e37ebbd29687e28de4d4e58c9a9ed1cd3a5309
Charlie Lor <lorcharlie@gmail.com>
2016-03-17 13:20:22 -0500

    Removed DefaultController and associated files and added IndexController and its test

---
+++ src/AppBundle/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexController extends Controller {
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction() {
+        return new Response();
+    }
+}

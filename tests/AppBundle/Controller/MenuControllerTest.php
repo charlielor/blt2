5458f327ff5001b122344ba6e9466f77cdc96b3c
Charlie Lor <lorcharlie@gmail.com>
2016-03-21 15:07:27 -0500

    Changed report to reporting, added functional tests for the links inside menu.html.twig, and changed the buttons in menu to links

--- tests/AppBundle/Controller/MenuControllerTest.php
+++ tests/AppBundle/Controller/MenuControllerTest.php
@@ -12,12 +12,36 @@ class MenuControllerTest extends WebTestCase {
 
         $crawler = $client->request('GET', '/');
 
-        # Testing response code for menu
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        # Testing response code for /
+        $this->assertTrue($client->getResponse()->isSuccessful());
 
-        # Testing buttons in menu
-        $this->assertContains('Receiving', $crawler->filter('#receiving')->text());
-        $this->assertContains('Delivering', $crawler->filter('#delivering')->text());
-        
+        # View link
+        $client->click($crawler->selectLink("View Packages")->link());
+//        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals('/view', $client->getRequest()->getRequestUri());
+        $client->back();
+
+        # Maintenance link
+        $client->click($crawler->selectLink("Maintenance")->link());
+//        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals('/maintenance', $client->getRequest()->getRequestUri());
+        $client->back();
+
+        # Reporting link
+        $client->click($crawler->selectLink("Reporting")->link());
+//        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals('/reporting', $client->getRequest()->getRequestUri());
+        $client->back();
+
+        # Receiving link
+        $client->click($crawler->selectLink("Receiving")->link());
+//        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals('/receiving', $client->getRequest()->getRequestUri());
+        $client->back();
+
+        # Delivering link
+        $client->click($crawler->selectLink("Delivering")->link());
+//        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals('/delivering', $client->getRequest()->getRequestUri());
     }
 }

2e90620615b374dbbc5b0005e0b30e1012a5caf1
Charlie Lor <lorcharlie@gmail.com>
2016-03-22 11:53:50 -0500

    Started on Package and PackingSlip entity and unit tests

--- tests/AppBundle/Entity/PackageEntityTest.php
+++ tests/AppBundle/Entity/PackageEntityTest.php
@@ -16,5 +16,27 @@ class PackageEntityTest extends \PHPUnit_Framework_TestCase
         $receiver = new Receiver("Office", 111);
 
         $package = new Package("12345", 4, $shipper, $receiver, $vendor, "PackageTest");
+
+        $this->assertEquals("12345", $package->getTrackingNumber());
+        $this->assertEquals(4, $package->getNumberOfPackages());
+        $this->assertEquals($vendor, $package->getVendor());
+        $this->assertEquals($shipper, $package->getShipper());
+        $this->assertEquals($receiver, $package->getReceiver());
+        $this->assertEquals("PackageTest", $package->getUserWhoReceived());
+        $this->assertNotEmpty($package->getDateModified());
+
+        $this->assertFalse($package->getDelivered());
+        $this->assertNull($package->getDateDelivered());
+        $this->assertNull($package->getUserWhoDelivered());
+
+        $this->assertFalse(false, $package->getPickedUp());
+        $this->assertNull($package->getDatePickedUp());
+        $this->assertNull($package->getUserWhoPickedUp());
+        $this->assertNull($package->getUserWhoAuthorizedPickUp());
+
+
+        $this->assertIsInstanceOf(Package::class, $package);
     }
+
+
 }
---
+++ tests/AppBundle/Entity/PackingSlipEntityTest.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Tests\AppBundle\Entity;
+
+use AppBundle\Entity\PackingSlip;
+
+class PackingSlipEntityTest extends \PHPUnit_Framework_TestCase
+{
+
+}

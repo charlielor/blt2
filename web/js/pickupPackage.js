340e00597258290e347a0d9041587f34bfd135fd
Charlie Lor <lorcharlie@gmail.com>
2016-05-20 15:26:33 -0500

    Finished pickup package related things over from blt1

--- web/js/pickupPackage.js
+++ web/js/pickupPackage.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
     var submitPackageTrackingNumber = $("#submitPackageTrackingNumber");
 
     var pickupPackageResultsModal = $("#pickupPackageResultsModal");
+    var pickupPackageResultsLabel = $("#pickupPackageResultsLabel");
+
     var userWhoPickedUpDiv = $("#userWhoPickedUpDiv");
     var userWhoAuthorizedDiv = $("#userWhoAuthorizedDiv");
 
@@ -62,15 +64,23 @@ $(document).ready(function() {
         pickupTrackingNumber.focus();
     });
 
-    submitPackageTrackingNumber.on("click", function() {
-        // Clear any errors
-        clearErrors("input");
+    pickupPackageModal.on("hide.bs.modal", function() {
+        clearErrors();
+        pickupTrackingNumber.val("");
+    });
 
+    pickupPackageResultsModal.on("hide.bs.modal", function() {
+        clearErrors();
+        dataTablePickUp.clear().draw();
+        userWhoPickedUp.val("");
+    });
+
+    submitPackageTrackingNumber.on("click", function() {
         // Get the pickup tracking number text
         var ptn = pickupTrackingNumber.val();
 
         // If the barcode is not empty, submit the barcode to the server and get the package
-        if ((ptn.replace(/\s/g,"")) == "") {
+        if ((ptn.replace(/\s/g,"")).length == 0) {
             // If the barcode is empty, then display error, clear and focus input box
             addError("input", "Tracking number can not be empty");
             pickupTrackingNumber.focus();
@@ -116,13 +126,8 @@ $(document).ready(function() {
         if (dataTablePickUp.rows().data().length > 1) {
             addError("table", "Please find the package being picked up and delete the rest");
         } else {
-            clearErrors("result");
-
             // Get the user who is picking up the package
             var uwpu = userWhoPickedUp.val();
-
-            // Get the user who is authorizing the pick up
-            var uwa = userWhoAuthorized.val();
 
             // Get the tracking number for that package
             var trackingNumberInPickUp = dataTablePickUp.row(0).data()['trackingNumber'];
@@ -131,23 +136,19 @@ $(document).ready(function() {
 
             // If the user who is picking up the package's textbox is not empty or null, then update the server
             // that the package has been picked up.
-            if (((uwpu.replace(/\s/g, "")) == "") || (uwa == null)) {
-                addError("result", "Please enter in the first and last name of the person who is picking up package");
+            if ((uwpu.replace(/\s/g, "")).length == 0) {
+                addError("table", "Please enter in the first and last name of the person who is picking up package");
                 userWhoPickedUp.focus();
             } else {
-                clearErrors("result");
-
                 // Set up the parameters
                 var dataBeingSent = {
-                    userWhoPickedUp: uwpu,
-                    userWhoAuthorized: uwa,
-                    pickedUp: 1
+                    userWhoPickedUp: uwpu
                 };
 
                 // Send the AJAX request
                 $.ajax({
                         type: "PUT",
-                        url: "package/" + trackingNumberInPickUp + "/update",
+                        url: "package/" + trackingNumberInPickUp + "/pickup",
                         data: dataBeingSent
                     })
                     .done(function(results) {
@@ -156,6 +157,8 @@ $(document).ready(function() {
                         // If the results come back successful, create a noty to let the user know that it has been successfully updated on the server
                         // Else let the user know that it was unsuccessful or that there was a connection failure
                         if (results['result'] == 'success') {
+                            clearErrors();
+
                             // Close the pickup results dialog
                             pickupPackageResultsModal.modal('hide');
 
@@ -176,12 +179,12 @@ $(document).ready(function() {
                             }
 
                         } else {
-                            addError("result", results["message"]);
+                            addError("table", results["message"]);
                             userWhoPickedUp.focus();
                         }
                     })
                     .fail(function() {
-                        addError("result", 'There was a connection error; please try again');
+                        addError("table", 'There was a connection error; please try again');
                     });
             }
         }
@@ -214,26 +217,34 @@ $(document).ready(function() {
     }
 
     function addError(error, label) {
-        pickupPackageLabel.removeClass("label-primary");
-        pickupPackageLabel.addClass("label-danger");
-        pickupPackageLabel.text(label);
-
         if (error == "input") {
+            pickupPackageLabel.removeClass("label-primary");
+            pickupPackageLabel.addClass("label-danger");
+            pickupPackageLabel.text(label);
+            
             pickupPackageDiv.addClass("has-error");
         } else if (error == "table") {
-            $("#pickupResultsDiv").addClass("has-error");
+            pickupPackageResultsLabel.removeClass("label-primary");
+            pickupPackageResultsLabel.addClass("label-danger");
+            pickupPackageResultsLabel.text(label);
+            
+            $("#userWhoPickedUpDiv").addClass("has-error");
         }
 
     }
 
-    function clearErrors(error){
+    function clearErrors() {
         // Clear all errors
         pickupPackageLabel.removeClass("label-danger");
         pickupPackageLabel.addClass("label-primary");
         pickupPackageLabel.text("Package must not have been delivered or picked up");
 
+        pickupPackageResultsLabel.removeClass("label-danger");
+        pickupPackageResultsLabel.addClass("label-primary");
+        pickupPackageResultsLabel.text("Type the first and last name of the person who is picking up package");
+
         pickupPackageDiv.removeClass("has-error");
-        $("#pickupResultsDiv").removeClass("has-error");
+        $("#userWhoPickedUpDiv").removeClass("has-error");
     }
 });
 
